4e7a94928e2d2fe876588c21d88aa6bcc2b0cf80
Kruttika Nadig <kruttikanadig@Kruttikas-MacBook-Pro.local>
2020-04-21 22:46:31 +0530

    added general params & descriptions

--- config.py
+++ config.py
@@ -7,12 +7,11 @@ from manager import scraper_manager
 scraper_params = {
     "USER_ID": os.environ.get("SHARECHAT_USER_ID"),
     "PASSCODE": os.environ.get("SHARECHAT_PASSWORD"), 
-    "tag_hashes": ["5anPZA", "3NZwyK", "3NE91Z", "X6Pxqx", # tags to scrape
-    "pkArae", "grGlpz", "GEpNKd", "0eXrv", "Kyweq"],
+    "tag_hashes": [],
     "content_to_scrape": "virality", # trending / fresh / virality / ml
     "pages": 2, # used when content_to_scrape == trending / fresh / ml
-    "unix_timestamp": "1587034967", # used when content_to_scrape == fresh
-    "data_path": "/Users/kruttikanadig/Documents/Tattle/sharechat-scraper/sharechat_data.csv", # used when content_to_scrape == virality
+    "unix_timestamp": "", # used when content_to_scrape == fresh
+    "data_path": "", # used when content_to_scrape == virality
     }
 
 
