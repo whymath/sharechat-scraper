7f79ae99e82a6ccf6a1536611160ef22a6d0d664
Kruttika Nadig <kruttikanadig@Kruttikas-MacBook-Pro.local>
2020-05-14 17:35:45 +0530

    update test app

--- server.py
+++ server.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import s3_mongo_helper
+
 app = Flask(__name__)
 
 @app.route('/scs/health')
 def health():
     return 'health'
+
+
+@app.route('/scs/metadata', methods=['POST'])
+def get_metadata():
+    data = request.json
+    #print(data)
+    #print(data["doc_id"])
+
+    filename=data["doc_id"]
+    #print(filename)
+    coll = s3_mongo_helper.initialize_mongo()
+    print(coll)
+    result = " "
+    for i in coll.find({"filename": filename}):
+        result = i
+        result.pop('_id', None)
+        print(result)
+    return jsonify(result)
+    #return("hi")
+
+@app.route('/scrape-daily')
+def daily_task():
+    return('done')
