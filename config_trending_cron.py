6e25d662d732d159de76f705546a96ed7b3f2a2a
denny <denny.george90@gmail.com>
2020-06-18 08:06:25 +0530

    (feat) add different config files to allow different modes of scraper

---
+++ config_trending_cron.py
@@ -0,0 +1,22 @@
+from manager import scraper_manager
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+scraper_params = {
+    "USER_ID": os.environ.get("SHARECHAT_USER_ID"),
+    "PASSCODE": os.environ.get("SHARECHAT_PASSWORD"),
+    "tag_hashes": ["5anPZA", "Kyweq", "njWVe", "6aj68", "e5jM8", "0eXrv"],
+    "bucket_ids": [],
+    "content_to_scrape": "trending",
+    "pages": 1,
+    "unix_timestamp": "",
+    "data_path": "",
+    "mode": "local",
+    "targeting": "tag",
+    "is_cron_job": True
+}
+
+
+if __name__ == "__main__":
+    scraper_manager(scraper_params)
