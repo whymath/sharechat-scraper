eeb1eb86ac9c06f51d18eb724e44ceb129b3512d
Kruttika Nadig <kruttikanadig@Kruttikas-MacBook-Pro.local>
2020-06-22 16:28:53 +0530

    bengali & rajasthani added

--- config_trending_cron.py
+++ config_trending_cron.py
@@ -6,7 +6,8 @@ load_dotenv()
 scraper_params = {
     "USER_ID": os.environ.get("SHARECHAT_USER_ID"),
     "PASSCODE": os.environ.get("SHARECHAT_PASSWORD"),
-    "tag_hashes": ["5anPZA", "Kyweq", "njWVe", "6aj68", "e5jM8", "0eXrv"],
+    "tag_hashes": ["5anPZA", "Kyweq", "njWVe", "6aj68", "e5jM8", "0eXrv", "EXG63e",
+    "Dek4p", "PP0Vz", "n4xXd", "jRRG6k", "B04WOk", "grNEGQ", "0z1aV3"],
     "bucket_ids": [],
     "content_to_scrape": "trending",
     "pages": 1,
