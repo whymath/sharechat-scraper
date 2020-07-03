f3023d73649639efa174b3c8a5ce6e75982c5ca9
Kruttika Nadig <kruttikanadig@Kruttikas-MacBook-Pro.local>
2020-07-03 16:48:10 +0530

    consolidated into pipeline_helper.py

--- examples/text_translation.py
+++
@@ -1,8 +0,0 @@
-import googletrans
-from googletrans import Translator
-
-def initialize_translator():
-    return Translator()
-
-def translate_text(text, translator):
-    return translator.translate(text).text
